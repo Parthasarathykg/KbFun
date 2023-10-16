68e5ea9812121e626441318ecf96c50d90b571c2
Parthasarathy KG <parthasarathy.gopinath@astrazeneca.com>
2023-10-17 00:40:03 +0530

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Birthday Countdown</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="countdown">
+        <h1>Countdown to 30th Birthday</h1>
+        <div id="timer"></div>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,20 @@
+// Set the date we're counting down to
+const birthday = new Date("YYYY-MM-DDT00:00:00");
+
+// Update the countdown every second
+const timer = setInterval(function () {
+    const now = new Date();
+    const timeRemaining = birthday - now;
+
+    if (timeRemaining <= 0) {
+        clearInterval(timer);
+        document.getElementById("timer").innerHTML = "Happy 30th Birthday!";
+    } else {
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById("timer").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+}, 1000);
